1230ed2e15857ebd2ba3e2ab4c6c203bad696a0a
Ruben Cancino <rubencancino6@gmail.com>
2014-09-25 17:06:33 -0500

    Ajustes incentivo 2

--- grails-app/controllers/com/luxsoft/sw4/ReporteController.groovy
+++ grails-app/controllers/com/luxsoft/sw4/ReporteController.groovy
@@ -3,10 +3,12 @@ package com.luxsoft.sw4
 import org.codehaus.groovy.grails.plugins.jasper.JasperExportFormat
 import org.codehaus.groovy.grails.plugins.jasper.JasperReportDef
 import grails.validation.Validateable
+import grails.plugin.springsecurity.annotation.Secured
 
 import com.luxsoft.sw4.cfdi.ImporteALetra
 import com.luxsoft.sw4.rh.*
 
+@Secured(['ROLE_ADMIN','RH_USER'])
 class ReporteController {
     static scaffold = true
 	
